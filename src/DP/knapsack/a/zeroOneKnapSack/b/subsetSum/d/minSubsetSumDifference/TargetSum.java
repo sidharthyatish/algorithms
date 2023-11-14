ff421337162cc82b1bf6dd0b35611a958da03b02
Sidharth Yatish M <yatishsidharth@gmail.com>
2023-11-14 12:45:16 +0530

    Add target sum problem

---
+++ src/DP/knapsack/a/zeroOneKnapSack/b/subsetSum/d/minSubsetSumDifference/TargetSum.java
@@ -0,0 +1,32 @@
+package DP.knapsack.a.zeroOneKnapSack.b.subsetSum.d.minSubsetSumDifference;
+
+import DP.knapsack.a.zeroOneKnapSack.b.subsetSum.c.count.SubSetSumCountBottomUp;
+
+class TargetSum {
+
+    SubSetSumCountBottomUp subSetSumCount;
+    
+    TargetSum(){
+        subSetSumCount = new SubSetSumCountBottomUp();
+    }
+
+    int targetSumCount(int[] arr, int sum){
+        /* Given an array, apply signs +, - such that their sum is equal to given sum
+         We need to count the number of such possibilities
+         This is the same as subsetsumdiffcount.
+        arr [1,1,2,3], sum = 1. This can be achieved in multiple ways [-1 +1 -2 +3], [-1 +1 -2 +3], [+1 +1 +2 -3]
+        say for a given result [-1 +1 -2 +3] this can be rewritten as [+1 +3 -1 -2] (bringing +s and -s together)
+        [1+3] - [1+2] ==> (s1 - s2)
+
+        thus {[-1 +1 -2 +3] == given sum} ==== {s1-s2 == given sum}
+        Thus this question is a rephrase of the problem that find subsets count whose difference is equal to a given number (SubSetSumDiffCount)
+        */
+       int totalSum = 0;
+       for(int eachNum : arr) totalSum+=eachNum;
+
+       int requiredSum = (diff+totalSum)/2;
+       return subSetSumCount.numberOfSubSets(arr,requiredSum,arr.length);
+    }
+
+
+}
