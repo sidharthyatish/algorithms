ee6e036acc2f35b3758de5a6711a94e62a534777
Sidharth Yatish M <yatishsidharth@gmail.com>
2024-03-03 19:00:56 +0530

    Sorting using recusrion
    Inspired from Aditya verma's https://youtu.be/AZ4jEY_JAVc?si=5XFiZ1vJXCMOQycL

---
+++ src/Recursion/SortUsingRecursion.java
@@ -0,0 +1,46 @@
+package Recursion;
+
+public class SortUsingRecursion {
+    //insert an element with an array containing n-1 values already in it.
+    public static void insert(int[] arr, int n, int elem){
+        if(n==0 || elem > arr[n-1]){
+            arr[n] = elem;
+            return;
+        }
+        int lastElem = arr[n-1];
+        insert(arr,n-1,elem);
+        arr[n]=lastElem;
+    }
+
+    public static void sort(int[] arr, int n){
+        if(n<=1) return;
+        int lastElem = arr[n-1];
+        //sort remaining elements
+        sort(arr,n-1);
+        //insert into the array of remaining elements. If its bigger element it will be any ways inserted in the last pos
+        insert(arr,n-1,lastElem);
+    }
+    public static void main(String[] args) {
+        //Testing insert on sorted array
+        int sortedArray[] = {8,10,11,13,15,0}; //last element is zero to leave for the filling position
+        insert(sortedArray,sortedArray.length-1,14);
+
+        printArray(sortedArray);
+
+        //Test sort method
+        int arr[] = {3,2,6,4,5};
+        sort(arr,arr.length);
+        printArray(arr);
+
+
+
+    }
+
+    private static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int elem : arr){
+            System.out.print(elem+" ");
+        }
+        System.out.println("]");
+    }
+}
