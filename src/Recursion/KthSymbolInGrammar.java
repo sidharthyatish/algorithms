a1c14434c5fe502674d4435fc3d470896139dd0e
Sidharth Yatish M <yatishsidharth@gmail.com>
2024-03-03 22:03:53 +0530

    kth symbol code fix

--- src/Recursion/KthSymbolInGrammar.java
+++ src/Recursion/KthSymbolInGrammar.java
@@ -4,7 +4,7 @@ public class KthSymbolInGrammar
 {
     public int kthGrammar(int n, int k) {
         if(k==1) return 0;
-        int length = (int)Math.pow(2,n-1)/2; //Note length is obtained by previous row's n and not current row's
+        int length = (int)Math.pow(2,n-1); //Note length is obtained by previous row's n and not current row's
         int mid = length/2;
         if(k<=mid)
             return kthGrammar(n-1,k);
