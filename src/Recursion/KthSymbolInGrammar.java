4997e0c1689bc97742e508a59bee37f365d1c9ef
Sidharth Yatish M <yatishsidharth@gmail.com>
2024-03-03 22:00:12 +0530

    kth symbol in grammar accepted code - fixed

--- src/Recursion/KthSymbolInGrammar.java
+++ src/Recursion/KthSymbolInGrammar.java
@@ -2,14 +2,13 @@ package Recursion;
 
 public class KthSymbolInGrammar
 {
-    int kthSymbol(int n, int k){
-        if(k==1||n==1) return 0;
-        int length = (int) Math.pow(2,n);
+    public int kthGrammar(int n, int k) {
+        if(k==1) return 0;
+        int length = (int)Math.pow(2,n-1)/2; //Note length is obtained by previous row's n and not current row's
         int mid = length/2;
-        if(k<=mid){
-            return kthSymbol(n-1,k);
-        }
+        if(k<=mid)
+            return kthGrammar(n-1,k);
         else
-            return kthSymbol(n-1,k-mid)==1?0:1;
+            return kthGrammar(n-1,k-mid) ==0? 1:0; 
     }
 }
