81513a2d74cd2f51ef26c8b137c5c5959014811b
Sidharth Yatish M <yatishsidharth@gmail.com>
2024-04-16 09:55:52 +0530

    peak element comments

--- src/BinarySearch/NotExactlySorted/PeakElement.java
+++ src/BinarySearch/NotExactlySorted/PeakElement.java
@@ -11,6 +11,16 @@ class PeakElement{
 
         To rise and fall the element should be between 0>element<n-1 
         0[------]n-1 only then it has space to rise and fall
+
+        How do we discard array
+
+        say the array
+
+        0 1 2 3 4 2 1 0
+              | 
+              -> say this is mid
+              now mid-1 is smaller and mid+1 is larger, which means the array is rising on right side
+              this means that peak element could be only on the rising side. Thus discard the non rising side
         
         if we say arrive in mid at 0, which means high and low was very close to 0.
             - This means the array is  peaked in 0 or 1 positions 
